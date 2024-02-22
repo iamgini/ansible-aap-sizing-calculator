eac41af3971ee199fe716a096661d6a83b33f3f2
Gineesh Madapparambath <net.gini@gmail.com>
2024-02-22 20:21:47 +0800

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculate() {
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var result = num1 + num2;
+    document.getElementById('result').textContent = 'Result: ' + result;
+  }
+  
